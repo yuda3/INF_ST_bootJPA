5271553cd74be189ce8bcd8f082dd83578b91992
yuda3 <yuda3@hotmail.com>
2024-06-19 23:52:35 +0900

    bootJPA Project スタート

---
+++ src/test/java/com/group/bootjpa/BootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.bootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
